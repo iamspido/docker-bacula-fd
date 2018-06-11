edb06e21542b6e301061d36b8bae5b750028aff1
iamspido <t.hohmann@raphael-gmbh.de>
2018-06-11 11:28:37 +0200

    Create mysqldump.sh

---
+++ mysqldump.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BACKUP_DIR="/var/backups/mysql/"
+MYSQL_USER="${2:-root}"
+MYSQL=/usr/bin/mysql
+MYSQL_PASSWORD="${1}"
+MYSQLDUMP=/usr/bin/mysqldump
+
+databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+for db in $databases; do
+$MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --quote-names --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$BACKUP_DIR/$db.bz2"
+done
