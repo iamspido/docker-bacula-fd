882877f52d1feaf0d3ec8c718d368ae8c106ac35
iamspido <t.hohmann@raphael-gmbh.de>
2018-07-04 15:59:27 +0200

    Update mysqldump.sh

--- mysqldump.sh
+++ mysqldump.sh
@@ -6,7 +6,7 @@ MYSQL=/usr/bin/mysql
 MYSQL_PASSWORD="${1}"
 MYSQLDUMP=/usr/bin/mysqldump
 
-databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD --protocol=socket -S /var/run/mysqld/mysqld.sock -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
 $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --quote-names --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$BACKUP_DIR/$db.bz2"
