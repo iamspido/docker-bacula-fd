9c920178ad657004d718bae6526b5d1bc17e8de0
Timo Hohmann <t.hohmann@raphael-gmbh.de>
2019-07-31 13:27:18 +0200

    remove mysql and sys from databasedump

--- ext-mysqldump.sh
+++ ext-mysqldump.sh
@@ -11,5 +11,9 @@ MYSQL_PORT="${MYSQL_PORT}"
 databases=`${MYSQL} --user=${MYSQL_USER} -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} --port=${MYSQL_PORT} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
-${MYSQLDUMP} --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} --quote-names --host=${MYSQL_HOST} --port=${MYSQL_PORT} --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "${BACKUP_DIR}/$db.bz2"
+	if [ ! [ $db = "mysql" && $db = "sys" ]]; then
+		${MYSQLDUMP} --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} --quote-names --host=${MYSQL_HOST} \
+		--port=${MYSQL_PORT} --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db \
+		| bzip2 > "${BACKUP_DIR}/$db.bz2"
+	fi
 done
