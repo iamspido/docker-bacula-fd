da1f4b51c3bf3b96dfc0e6eef7b3d874de613373
iamspido <t.hohmann@raphael-gmbh.de>
2020-01-27 10:26:14 +0100

    rename envs

--- ext-mysqldump.sh
+++ ext-mysqldump.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-BACKUP_DIR="${BACKUP_DIR}"
-MYSQL_USER="${MYSQL_USER}"
-MYSQL=/usr/bin/mysql
-MYSQL_PASSWORD="${MYSQL_PASSWORD}"
-MYSQLDUMP=/usr/bin/mysqldump
-MYSQL_HOST="${MYSQL_HOST}"
-MYSQL_PORT="${MYSQL_PORT}"
+ENV_BACKUP_DIR="${ENV_BACKUP_DIR}"
+ENV_MYSQL_USER="${ENV_MYSQL_USER}"
+ENV_MYSQL=/usr/bin/mysql
+ENV_MYSQL_PASSWORD="${ENV_MYSQL_PASSWORD}"
+ENV_MYSQLDUMP=/usr/bin/mysqldump
+ENV_MYSQL_HOST="${ENV_MYSQL_HOST}"
+ENV_MYSQL_PORT="${ENV_MYSQL_PORT}"
 
-databases=`${MYSQL} --user=${MYSQL_USER} -p${MYSQL_PASSWORD} --host=${MYSQL_HOST} --port=${MYSQL_PORT} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+databases=`${ENV_MYSQL} --user=${ENV_MYSQL_USER} -p${ENV_MYSQL_PASSWORD} --host=${ENV_MYSQL_HOST} --port=${ENV_MYSQL_PORT} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
 	if [[ ! $db = "mysql" && ! $db = "sys" ]]; then
-		${MYSQLDUMP} --force --opt --user=${MYSQL_USER} -p${MYSQL_PASSWORD} --quote-names --host=${MYSQL_HOST} \
-		--port=${MYSQL_PORT} --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db \
-		| bzip2 > "${BACKUP_DIR}/$db.bz2"
+		${ENV_MYSQLDUMP} --force --opt --user=${ENV_MYSQL_USER} -p${ENV_MYSQL_PASSWORD} --quote-names --host=${ENV_MYSQL_HOST} \
+		--port=${ENV_MYSQL_PORT} --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db \
+		| bzip2 > "${ENV_BACKUP_DIR}/$db.bz2"
 	fi
 done
