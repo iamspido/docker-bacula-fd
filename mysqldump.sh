408d384d6755084782d58f3982840565fb79d57e
iamspido <t.hohmann@raphael-gmbh.de>
2020-01-27 10:23:45 +0100

    Rename envs

--- mysqldump.sh
+++ mysqldump.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BACKUP_DIR="/mount/var/backups/mysql/"
-MYSQL_USER="${MYSQL_USER}"
-MYSQL=/usr/bin/mysql
-MYSQL_PASSWORD="${MYSQL_PASSWORD}"
-MYSQLDUMP=/usr/bin/mysqldump
+ENV_BACKUP_DIR="/mount/var/backups/mysql/"
+ENV_MYSQL_USER="${ENV_MYSQL_USER}"
+ENV_MYSQL=/usr/bin/mysql
+ENV_MYSQL_PASSWORD="${ENV_MYSQL_PASSWORD}"
+ENV_MYSQLDUMP=/usr/bin/mysqldump
 
-databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD --protocol=socket -S /var/run/mysqld/mysqld.sock -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+databases=`$ENV_MYSQL --user=$ENV_MYSQL_USER -p$ENV_MYSQL_PASSWORD --protocol=socket -S /var/run/mysqld/mysqld.sock -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
-$MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --quote-names --protocol=socket -S /var/run/mysqld/mysqld.sock --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$BACKUP_DIR/$db.bz2"
+$ENV_MYSQLDUMP --force --opt --user=$ENV_MYSQL_USER -p$ENV_MYSQL_PASSWORD --quote-names --protocol=socket -S /var/run/mysqld/mysqld.sock --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$ENV_BACKUP_DIR/$db.bz2"
 done
