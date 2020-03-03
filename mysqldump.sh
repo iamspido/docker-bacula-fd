714b7c4367ab846a2d5b7f264d32bcb4681138c8
iamspido <t.hohmann@raphael-gmbh.de>
2020-03-03 15:19:49 +0100

    add check if mysqldump was successful and give succcessful output

--- mysqldump.sh
+++ mysqldump.sh
@@ -9,5 +9,15 @@ ENV_MYSQLDUMP=/usr/bin/mysqldump
 databases=`$ENV_MYSQL --user=$ENV_MYSQL_USER -p$ENV_MYSQL_PASSWORD --protocol=socket -S /var/run/mysqld/mysqld.sock -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
 
 for db in $databases; do
-$ENV_MYSQLDUMP --force --opt --user=$ENV_MYSQL_USER -p$ENV_MYSQL_PASSWORD --quote-names --protocol=socket -S /var/run/mysqld/mysqld.sock --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$ENV_BACKUP_DIR/$db.bz2"
+  $ENV_MYSQLDUMP --force --opt --user=$ENV_MYSQL_USER -p$ENV_MYSQL_PASSWORD --quote-names --protocol=socket -S /var/run/mysqld/mysqld.sock --single-transaction --ignore-table=mysql.event --quick --max_allowed_packet=512M --databases $db | bzip2 > "$ENV_BACKUP_DIR/$db.bz2"
+  if [ ! $? -eq 0 ]; then
+    echo "Backup failed!"
+    exit 1
+  fi
 done
+LEN=$(echo "$databases" | wc -l)
+if (( $LEN > 1 )); then
+  echo "All $LEN backups were successful."
+else
+  echo "$LEN backup was successful."
+fi
